5d136611e5da18a7e4211f65970f66f328a89c05
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-19 17:34:28 +0600

    Menu Bar Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Menubar from './Components/Menubar/Menubar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App contanier">
+      <Menubar></Menubar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Menubar/Menubar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Menubar.css'
+
+const Menubar = () => {
+  return (
+    <div className="row">
+      <div className="col-md-4 logo">Logo</div>
+      <div className="col-md-8 ">
+        <ul>
+          <li className="menu">Home</li>
+          <li className="menu">About Us</li>
+          <li className="menu">Services</li>
+          <li className="menu">Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Menubar;
