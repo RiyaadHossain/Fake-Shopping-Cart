0456041647c409dc16a06a6466063b03231943df
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-19 20:55:58 +0600

    Yay, Problem fixed, time to add some animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
-import { useState } from "React";
 import Menubar from './Components/Menubar/Menubar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AllProducts from './Components/AllProducts/AllProducts';
+import { useState } from 'react';
 
 function App() {
   const [cart, setCart] = useState(0)
+
   const setCartCount = () => {
     setCart(cart + 1)
   }
   return (
     <div className="App">
       <Menubar cart={cart}></Menubar>
-      <AllProducts setCartCount ={setCartCount}></AllProducts>
+      <AllProducts setCartCount={setCartCount}></AllProducts>
     </div>
   );
 }
